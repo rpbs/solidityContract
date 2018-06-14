ac4c71d968617fcec652c406e4c57cd50b2089d8
Renyo <renyopaulo@gmail.com>
2018-06-14 16:02:53 -0300

    arquivo de teste
    bolo de pote

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,21 @@
 const asset = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+class Bolo {
+   tipo(){
+     return 'pote';
+   }
+  
+  sabor(){
+    return 'kitkat'
+  }
+}
+
+describe('Teste do destemido bolo de pot3', () => {
+  it('tem que ser bolot de pote', () => {
+    const bolo = new Bolo();
+    assert.equal(bolo.tipo(), 'pote');
+  });
+});
