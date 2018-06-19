beaa7a89183cda340707ab66be9f876e6a2a8b19
rpbs <renyopaulo@gmail.com>
2018-06-18 21:06:28 -0300

    construtor...

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.17;
 contract Inbox {
     string public message;
     
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
     function setMessage(string initialMessage) public {
         message = initialMessage;
     }
