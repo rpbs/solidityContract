9a63c6fbdaebc08411f7af4b89b8fd11bc446b50
rpbs <renyopaulo@gmail.com>
2018-06-18 21:06:49 -0300

    arquivo bacana de teste

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,21 +1,42 @@
-const asset = require('assert');
+const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
 
-class Bolo {
-   tipo(){
-     return 'pote';
-   }
-  
-  sabor(){
-    return 'kitkat'
-  }
-}
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-describe('Teste do destemido bolo de pot3', () => {
-  it('tem que ser bolot de pote', () => {
-    const bolo = new Bolo();
-    assert.equal(bolo.tipo(), 'pote');
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+const VALOR_INICIAL = 'aopa';
+
+beforeEach( async () =>{
+  accounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [VALOR_INICIAL] })
+    .send({ from: accounts[0], gas: 1000000 });
+});
+
+describe('Inbox', () => {
+  it('DEPLOY CONTRATO', () => {
+    assert.ok(inbox.options.address)
+  });
+
+  it('tem valor default', async () => {
+    const message = await inbox.methods.message().call();
+    assert.ok(message);
+    assert.equal(message, VALOR_INICIAL)
+  });
+
+  it('modificar valor', async() => {
+    await inbox.methods.setMessage('OPA').send({
+      from: accounts[0]
+    });
+    let msg = await inbox.methods.message().call();
+    assert.ok(msg);
+    assert.equal(msg, 'OPA');
   });
 });
