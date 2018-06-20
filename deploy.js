647c37514506cbf505e6fd9202bb0cda699a7523
rpbs <renyopaulo@gmail.com>
2018-06-19 21:04:37 -0300

    modificações do dia

--- deploy.js
+++ deploy.js
@@ -1,3 +1,25 @@
-const HdWallet = require('truffle-hdwallet-provider');
+const HDWallet = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytevode} = require('../compile');
+const { interface, bytecode} = require('./compile');
+
+// Tendo acesso a uma account...
+const provider = new HDWallet(
+    'six stairs become melody steel thunder truck march grunt roof toast steel',
+    'https://rinkeby.infura.io/9NWZODUWUqTOVKy6Oaws'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('tentando deployar a partir da conta', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ['Aopa'] })
+      .send({ from: accounts[0], gas: 1000000 });    
+
+      console.log('Contrato  no endereço', result.options.address);
+}
+
+deploy();
