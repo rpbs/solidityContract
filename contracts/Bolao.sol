84a593485b55d26343673ca3f2c82de090246576
Renyo <renyopaulo@gmail.com>
2018-06-21 09:09:08 -0300

    conteúdo
    conteúdo

--- contracts/Bolao.sol
+++ contracts/Bolao.sol
@@ -1,1 +1,15 @@
 
+contract Bolao {
+    address public manager;
+    address[] public pessoas;
+
+    function Bolao() public {
+        manager = msg.sender;
+    }
+
+    function entrar() public payable {
+        require(msg.value > 0.1 ether);
+        
+        pessoas.push(msg.sender);
+    }
+}
