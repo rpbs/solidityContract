9bbbcc0436e3f8d3a1af57f36b144b1960d06504
Renyo <renyopaulo@gmail.com>
2018-06-26 08:41:52 -0300

    Update compile.js

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const bolaoPath = path.resolve(__dirname, 'contracts', 'Bolao.sol');
 
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(bolaoPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Bolao'];
