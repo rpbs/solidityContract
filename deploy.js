fa648ec2ff0b49e21495197380084c605136060e
rpbs <renyopaulo@gmail.com>
2018-07-08 23:40:08 -0300

    console log

--- deploy.js
+++ deploy.js
@@ -17,8 +17,9 @@ const deploy = async () => {
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
       .deploy({ data: bytecode })
-      .send({ from: accounts[0], gas: 1000000 });    
+      .send({ from: accounts[0], gas: 1000000 });
 
+      console.log(interface);
       console.log('Contrato  no endereço', result.options.address);
 }
 
